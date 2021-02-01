bb6eee422d324e227fcce34519241444133f260b
maxdz-gmbh <maxdz@maxdz.gmbh>
2021-02-01 20:11:49 +0100

    v 0.1.1 added

---
+++ mdz_types.h
@@ -0,0 +1,371 @@
+/**
+ * \ingroup mdz libraries
+ *
+ * \author maxdz Software GmbH
+ *
+ * \par license
+ * This file is subject to the terms and conditions defined in file 'LICENSE.txt', which is part of this source code package.
+ *
+ * \par description
+ * Public types of mdz_containers library
+ *
+ * \par info
+ * See additional info on mdz_containers library like version, portability, etc in mdz_containers.h
+ */
+
+#ifndef MDZ_TYPES_H
+#define MDZ_TYPES_H
+
+#include <stddef.h>
+
+#include <stdint.h>
+#include <limits.h>
+
+#ifndef _WIN32
+#include <pthread.h>
+#endif
+
+ /**
+ * Boolean type of mdz_containers library.
+ */
+#define mdz_false 0
+#define mdz_true 1
+typedef unsigned char mdz_bool;
+
+#ifdef _WIN32
+typedef void* HANDLE;
+#endif
+
+/**
+* Shared data for asynchronous calls
+*/
+struct mdz_asyncData
+{
+  /**
+  * Pointer to string instance
+  */
+  void* m_pString;
+
+  /**
+  * Result of call. Invalid if call is not completely finished (m_bFinished is mdz_false)
+  */
+  size_t m_nResult;
+
+  /**
+  * Additional data returned by call (if any). Invalid if call is not completely finished (m_bFinished is mdz_false)
+  */
+  void* m_pData;
+
+  /**
+  * Thread on which the call is executed. May be used by client for wait operations
+  */
+#ifdef _WIN32
+  HANDLE m_hThread;
+#else
+  pthread_t m_hThread;
+#endif
+
+  /**
+  * mdz_true if the call is completely finished. Otherwise mdz_false (if interrupted/cancelled)
+  */
+  mdz_bool m_bFinished;
+
+  /**
+  * Should be set in mdz_true during call execution, to cancel the call. Otherwise mdz_false
+  */
+  mdz_bool m_bCancel;
+};
+
+/**
+ * Attachment type
+ */
+enum mdz_attach_type
+{
+  /**
+   * Size is 0 after attachment
+   */
+  MDZ_ATTACH_ZEROSIZE = 0,
+
+  /**
+   * Size is Capacity minus 1 after attachment; 0-terminator is present
+   */
+  MDZ_ATTACH_SIZE_TERMINATOR = 1,
+
+  /**
+   * Size is Capacity after attachment; without 0-terminator
+   */
+  MDZ_ATTACH_SIZE_NO_TERMINATOR = 2
+};
+
+/**
+  * Data endianness
+  */
+enum mdz_endianness
+{
+  /**
+   * Undefined endianness
+   */
+  MDZ_ENDIAN_UNDEFINED = 0,
+
+  /**
+   * Little-endian
+   */
+  MDZ_ENDIAN_LITTLE = 1,
+
+  /**
+   * Big-endian
+   */
+  MDZ_ENDIAN_BIG = 2,
+
+  /**
+   * Error endianness
+   */
+  MDZ_ENDIAN_ERROR = 3
+};
+
+/**
+ * Result of comparison
+ */
+enum mdz_compare_result
+{
+  /**
+   * Equal
+   */
+  MDZ_COMPARE_EQUAL = 0,
+
+  /**
+   * Non-equal
+   */
+  MDZ_COMPARE_NONEQUAL = 1,
+
+  /**
+   * Greater
+   */
+  MDZ_COMPARE_GREATER = 2,
+
+  /**
+   * Smaller
+   */
+  MDZ_COMPARE_SMALLER = 3,
+
+  /**
+   * Comparison ended with error
+   */
+  MDZ_COMPARE_ERROR = 4
+};
+
+/**
+ * Method of find
+ */
+enum mdz_find_method
+{
+  /*
+   * standard C-library implementation
+   */
+  MDZ_FIND_CLIB = 0,
+  
+  /**
+   * Optimized Boyer-Moore-Horspool search
+   */
+  MDZ_FIND_BMH = 1,
+
+  /**
+   * Improved "brute-force" search
+   */
+  MDZ_FIND_BRUTE = 2,
+
+  /**
+   * Optimized search for monotone sub-string in monotone string
+   */
+  MDZ_FIND_MONOTONE = 3
+};
+
+enum mdz_error_code
+{
+  /**
+   * No error
+   */
+  MDZ_ERROR_NONE = 0,
+
+  /**
+   * Invalid "data" parameter
+   */
+  MDZ_ERROR_DATA = 1,
+
+  /**
+   * Not enough capacity
+   */
+  MDZ_ERROR_CAPACITY = 2,
+
+  /**
+   * Invalid "offset" parameter
+   */
+  MDZ_ERROR_OFFSET = 3,
+
+  /**
+   * "Count" parameter is zero
+   */
+  MDZ_ERROR_ZEROCOUNT = 4,
+
+  /**
+   * "Count" parameter is too big
+   */
+  MDZ_ERROR_BIGCOUNT = 5,
+
+  /**
+   * "Left" parameter is too big
+   */
+  MDZ_ERROR_BIGLEFT = 6,
+
+  /**
+   * "Right" parameter is too big
+   */
+  MDZ_ERROR_BIGRIGHT = 7,
+
+  /**
+   * Invalid "items" parameter
+   */
+  MDZ_ERROR_ITEMS = 8,
+
+  /**
+   * Container is empty
+   */
+  MDZ_ERROR_EMPTY = 9,
+
+  /**
+   * Container is not empty
+   */
+  MDZ_ERROR_NONEMPTY = 10,
+
+  /**
+   * Invalid "sub-container" parameter
+   */
+  MDZ_ERROR_SUBCONTAINER = 11,
+
+  /**
+   * Container is attached
+   */
+  MDZ_ERROR_ATTACHED = 12,
+
+  /**
+   * Memory allocation error
+   */
+  MDZ_ERROR_ALLOCATION = 13,
+
+  /**
+   * Data content error
+   */
+  MDZ_ERROR_CONTENT = 14,
+
+  /**
+   * Invalid endiannes type
+   */
+  MDZ_ERROR_ENDIANNESS = 15,
+
+  /**
+   * Invalid attach typpe
+   */
+  MDZ_ERROR_ATTACHTYPE = 16,
+
+  /**
+   * Invalid find method
+   */
+  MDZ_ERROR_FINDMETHOD = 17,
+
+  /**
+   * Memory allocation for thread error
+   */
+  MDZ_ERROR_THREAD_ALLOC = 18,
+
+  /**
+   * Thread start error
+   */
+  MDZ_ERROR_THREAD_START = 19,
+
+  /**
+  * Invalid "source string" parameter
+  */
+  MDZ_ERROR_SOURCE = 20
+};
+
+/**
+* Handle of ANSI string in mdz_string library
+*/
+struct mdz_Ansi
+{
+  /**
+  * Pointer to data of string. Do not change it directly.
+  */
+  char* m_pData;
+
+  /**
+  * Error code if operation failed or there are some inconsistences.
+  */
+  enum mdz_error_code m_enErrorCode;
+};
+
+/**
+* Handle of utf8 string in mdz_string library
+*/
+struct mdz_Utf8
+{
+  /**
+  * Pointer to data of string. Do not change it directly.
+  */
+  unsigned char* m_pData;
+
+  /**
+  * Error code if operation failed or there are some inconsistences.
+  */
+  enum mdz_error_code m_enErrorCode;
+};
+
+/**
+* Handle of UTF-16 string in mdz_string library
+*/
+struct mdz_Utf16
+{
+  /**
+  * Pointer to data of string. Do not change it directly.
+  */
+  const uint16_t* m_pData;
+
+  /**
+  * Error code if operation failed or there are some inconsistences.
+  */
+  enum mdz_error_code m_enErrorCode;
+};
+
+/**
+* Handle of UTF-32 string in mdz_string library
+*/
+struct mdz_Utf32
+{
+  /**
+  * Pointer to data of string. Do not change it directly.
+  */
+  const uint32_t* m_pData;
+
+  /**
+  * Error code if operation failed or there are some inconsistences.
+  */
+  enum mdz_error_code m_enErrorCode;
+};
+
+/**
+* Handle of "wide"-character string in mdz_string library
+*/
+struct mdz_Wchar
+{
+  /**
+  * Pointer to data of string. Do not change it directly.
+  */
+  const wchar_t* m_pData;
+
+  /**
+  * Error code if operation failed or there are some inconsistences.
+  */
+  enum mdz_error_code m_enErrorCode;
+};
+
+#endif
---
+++ mdz_unicode.h
@@ -0,0 +1,70 @@
+/**
+* \ingroup mdz_unicode library
+*
+* \author maxdz Software GmbH
+*
+* \par license
+* This file is subject to the terms and conditions defined in file 'LICENSE.txt', which is part of this source code package.
+*
+* \par description
+* mdz_unicode initialization functions.
+*
+* \par portability
+* Source code of library conforms to ANSI C 89/90 Standard.
+*
+* \version 1.0.0
+*
+* \date 2020-10
+*
+*/
+
+#ifndef MDZ_UNICODE_H
+#define MDZ_UNICODE_H
+
+#include "mdz_types.h"
+
+#ifdef __cplusplus
+extern "C"
+{
+#endif
+
+/**
+ * \defgroup Init functions
+ */
+
+/**
+ * Initializes unicode library. This function should be caled before any other function of the library.
+ * \param pFirstNameHash - user first name hash code
+ * \param pLastNameHash - user last name hash code
+ * \param pEmailHash - user e-mail hash code
+ * \param pLicenseHash - license hash code
+ * \return:
+ * mdz_true - if the initialization has succeed, otherwise false
+ */
+mdz_bool mdz_unicode_init(const uint32_t* pFirstNameHash, const uint32_t* pLastNameHash, const uint32_t* pEmailHash, const uint32_t* pLicenseHash);
+
+/**
+ * Initializes unicode library. This function should be caled before any other function of the library.
+ * Memory for license data starts at position pStart. Size of internal initialization structure is returned in pSize.
+ * \param pFirstNameHash - user first name hash code
+ * \param pLastNameHash - user last name hash code
+ * \param pEmailHash - user e-mail hash code
+ * \param pLicenseHash - license hash code
+ * \param pStart - memory start position of license data
+ * \param nAreaSize - size of available memory from pStart in bytes. Should be large enough for license data (> 500 bytes)
+ * \param pOutSize - actual size of placed license data in bytes
+ * \return:
+ * mdz_true - if the initialization has succeed, otherwise false
+ */
+mdz_bool mdz_unicode_init_attached(const uint32_t* pFirstNameHash, const uint32_t* pLastNameHash, const uint32_t* pEmailHash, const uint32_t* pLicenseHash, const char* pStart, size_t nAreaSize, size_t* pOutSize);
+
+/**
+ * Un-initializes unicode library and frees corresponding memory allocations.
+ */
+void mdz_unicode_uninit(void);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
